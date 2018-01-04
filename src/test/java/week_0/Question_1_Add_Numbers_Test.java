7ee133142b23e4794d211075163e194be617ad89
clara <clara@clara.com>
2018-01-04 13:08:54 -0600

    Another test for add numbers

--- src/test/java/week_0/Question_1_Add_Numbers_Test.java
+++ src/test/java/week_0/Question_1_Add_Numbers_Test.java
@@ -13,6 +13,7 @@ public class Question_1_Add_Numbers_Test  {
         assertEquals("Adding the numbers 1 and 100 should return 101", 101, addNumbers(1, 100));
         assertEquals("Adding the numbers 6 and 10 should return 16", 16, addNumbers(6, 10));
         assertEquals("Adding the numbers -6 and -100 should return -106", -106, addNumbers(-6, -100));
+        assertEquals("Adding the numbers 3 and 3 should return 6", 6, addNumbers(3, 3));
     
     }
     
